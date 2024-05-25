9b6683a245a30dd01461f64f0977fc5f1706064c
Gavin Downard <me@floyza.com>
2024-05-25 04:58:37 +0000

    Fix player id
    Are there no warnings for missing a struct entry?

--- src/actor.cpp
+++ src/actor.cpp
@@ -3,7 +3,8 @@
 #include "colors.hpp"
 
 Actor create_player(int id, Pos p) {
-  return Actor{.p = p, .chr = '@', .color = col::WHITE_BR, .is_player = true, .hp = 20, .max_hp = 20, .atk = 3};
+  return Actor{
+      .id = id, .p = p, .chr = '@', .color = col::WHITE_BR, .is_player = true, .hp = 20, .max_hp = 20, .atk = 3};
 }
 
 Actor generate_monster(int id, Breed b, Pos p) {
